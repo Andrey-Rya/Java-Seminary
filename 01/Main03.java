5b0b0b7a2476347cb89c16e5e31a4402c7864e22
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2022-12-15 09:06:05 +0700

    Update Main03.java

--- 01/Main03.java
+++ 01/Main03.java
@@ -47,3 +47,43 @@ public class Main {
         return ans;
     }
 }
+
+
+/*
+Второй вариант простого калькулятора
+*/
+
+public class Main03 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "UTF-8");
+        System.out.println("Введите два числа через пробел");
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        System.out.print(
+                "Какую математическую операцию вы хотите произвести: \n сложение - sum \n" +
+                        " вычитание - minus \n умножение - mult \n деление - div \n Введите: ");
+
+        String operation = sc.next();
+        sc.close();
+
+        double res = 0;
+
+        if (operation.equals("sum")) {
+            res = x + y;
+            System.out.println(res);
+        } else if (operation.equals("minus")) {
+            res = x - y;
+        } else if (operation.equals("mult")) {
+            res = x * y;
+        } else if (operation.equals("div")) {
+            res = x / y;
+        }
+
+        System.out.println(operation);
+
+        System.out.println(res);
+    }
+
+}
